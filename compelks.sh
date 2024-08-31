ce4409206f9857b245cc7804dfd8aefd5cc1bf14
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-08-31 11:55:45 +0200

    speed up drawing

--- compelks.sh
+++ compelks.sh
@@ -1,4 +1,5 @@
-CCOPTSS="-os -bt=none -0 -zq -s -mm -wx -zastd=c99 -zls"
+CCOPTSS="-os             -bt=none -0 -zq -s -mm -wx -zastd=c99 -zls"
+CCOPTST="-oaxet -oh -ol+ -bt=none -0 -zq -s -mm -wx -zastd=c99 -zls"
 
 wcc $CCOPTSS am_map.c   -fo=am_map.obj
 wcc $CCOPTSS d_items.c  -fo=d_items.obj
@@ -9,7 +10,6 @@ wcc $CCOPTSS hu_stuff.c -fo=hu_stuff.obj
 wcc $CCOPTSS i_audio.c  -fo=i_audio.obj
 wcc $CCOPTSS i_main.c   -fo=i_main.obj
 wcc $CCOPTSS i_elks.c   -fo=i_elks.obj
-wcc $CCOPTSS i_vmodey.c -fo=i_vmodey.obj
 wcc $CCOPTSS info.c     -fo=info.obj
 wcc $CCOPTSS m_cheat.c  -fo=m_cheat.obj
 wcc $CCOPTSS m_menu.c   -fo=m_menu.obj
@@ -32,7 +32,6 @@ wcc $CCOPTSS p_telept.c -fo=p_telept.obj
 wcc $CCOPTSS p_tick.c   -fo=p_tick.obj
 wcc $CCOPTSS p_user.c   -fo=p_user.obj
 wcc $CCOPTSS r_data.c   -fo=r_data.obj
-wcc $CCOPTSS r_draw.c   -fo=r_draw.obj
 wcc $CCOPTSS r_plane.c  -fo=r_plane.obj
 wcc $CCOPTSS r_sky.c    -fo=r_sky.obj
 wcc $CCOPTSS r_things.c -fo=r_things.obj
@@ -45,6 +44,9 @@ wcc $CCOPTSS w_wad.c    -fo=w_wad.obj
 wcc $CCOPTSS wi_stuff.c -fo=wi_stuff.obj
 wcc $CCOPTSS z_bmallo.c -fo=z_bmallo.obj
 wcc $CCOPTSS z_zone.c   -fo=z_zone.obj
+
+wcc $CCOPTST i_vmodey.c -fo=i_vmodey.obj
+wcc $CCOPTST r_draw.c   -fo=r_draw.obj
 
 owcc -bos2 -s -Wl,option -Wl,start=_start -Wl,option -Wl,dosseg -Wl,option -Wl,nodefaultlibs -Wl,option -Wl,stack=0x1000 -Wl,option -Wl,heapsize=0x1000 -Wl,library -Wl,$LIBC -o elksdoom.os2 am_map.obj d_items.obj d_main.obj f_finale.obj g_game.obj hu_stuff.obj i_audio.obj i_main.obj i_elks.obj i_vmodey.obj info.obj m_cheat.obj m_menu.obj m_random.obj p_doors.obj p_enemy.obj p_floor.obj p_inter.obj p_lights.obj p_map.obj p_maputl.obj p_mobj.obj p_plats.obj p_pspr.obj p_setup.obj p_sight.obj p_spec.obj p_switch.obj p_telept.obj p_tick.obj p_user.obj r_data.obj r_draw.obj r_plane.obj r_sky.obj r_things.obj s_sound.obj sounds.obj st_stuff.obj tables.obj v_video.obj w_wad.obj wi_stuff.obj z_bmallo.obj z_zone.obj
 
--- setenvwc.sh
+++ setenvwc.sh
@@ -1,4 +1,6 @@
+export ELKSTOPDIR=/home/frenkel/elks
 export WATCOM=/usr/bin/watcom
-export INCLUDE=/home/frenkel/elks/libc/include:/home/frenkel/elks/include:/home/frenkel/elks/elks/include:/usr/bin/watcom/h:$INCLUDE
-export LIBC=/home/frenkel/elks/libc/libc.lib
-export PATH=/usr/bin/watcom/binl64:/usr/bin/watcom/binl:$PATH
+
+export INCLUDE=$ELKSTOPDIR/libc/include:$ELKSTOPDIR/include:$ELKSTOPDIR/elks/include:$WATCOM/h:$INCLUDE
+export LIBC=$ELKSTOPDIR/libc/libc.lib
+export PATH=$WATCOM/binl64:$WATCOM/binl:$PATH
