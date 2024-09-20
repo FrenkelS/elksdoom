b977157d33c9bed1df72da743927e67aeb51e2cf
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-09-20 09:30:54 +0200

    change access permissions of compelks.sh

--- setenvwc.sh
+++ setenvwc.sh
@@ -4,5 +4,3 @@ export WATCOM=/usr/bin/watcom
 export INCLUDE=$ELKSTOPDIR/libc/include:$ELKSTOPDIR/include:$ELKSTOPDIR/elks/include:$WATCOM/h:$INCLUDE
 export LIBC=$ELKSTOPDIR/libc/libc.lib
 export PATH=$WATCOM/binl64:$WATCOM/binl:$PATH
-
-chmod -v 755 compelks.sh
