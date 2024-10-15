1f7fa2ca47dab7fbe837bcc97a5099221ad566fe
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2024-10-15 11:55:39 +0200

    step 2

--- i_video.h
+++ i_video.h
@@ -35,7 +35,7 @@ void I_FinishUpdate(void);
 
 
 void R_DrawColumn (const draw_column_vars_t *dcvars);
-void R_DrawColumnFlat(int16_t texture, const draw_column_vars_t *dcvars);
+void R_DrawColumnFlat(uint8_t color, const draw_column_vars_t *dcvars);
 void R_DrawFuzzColumn (const draw_column_vars_t *dcvars);
 
 
--- r_plane.c
+++ r_plane.c
@@ -28,13 +28,12 @@
 #include "compiler.h"
 #include "r_defs.h"
 #include "r_main.h"
+#include "i_video.h"
 
 #include "globdata.h"
 
 
-static int16_t firstflat;
-static int16_t  animated_flat_basepic;
-static int16_t __far* flattranslation;             // for global animation
+static int16_t nukage;
 
 
 //
@@ -53,67 +52,11 @@ static int16_t __far* flattranslation;             // for global animation
 byte R_GetPlaneColor(int16_t picnum, int16_t lightlevel)
 {
 	const uint8_t __far* colormap = R_LoadColorMap(lightlevel);
-	return colormap[flattranslation[picnum]];
-}
 
+	if (picnum == -3)
+		picnum = nukage;
 
-//
-// R_InitFlats
-//
-
-void R_InitFlats(void)
-{
-	       firstflat = W_GetNumForName("F_START") + 1;
-
-	int16_t lastflat = W_GetNumForName("F_END")   - 1;
-	int16_t numflats = lastflat - firstflat + 1;
-
-	// Create translation table for global animation.
-
-	flattranslation = Z_MallocStatic((numflats + 1) * sizeof(*flattranslation));
-
-	animated_flat_basepic = R_FlatNumForName("NUKAGE1");
-
-	byte    __far* source    = Z_MallocStatic( 64 * 64);
-	int16_t __far* histogram = Z_MallocStatic(256 * sizeof(int16_t));
-
-	for (int16_t i = 0; i < numflats; i++)
-	{
-		W_ReadLumpByNum(firstflat + i, source);
-
-		_fmemset(histogram, 0, 256 * sizeof(int16_t));
-		for (int16_t j = 0; j < 64 * 64; j++)
-			histogram[source[j]]++;
-
-		int16_t maxamount = 0;
-		int16_t maxindex  = 0;
-		for (int16_t j = 0; j < 256; j++)
-		{
-			if (histogram[j] > maxamount)
-			{
-				maxamount = histogram[j];
-				maxindex  = j;
-			}
-		}
-
-		flattranslation[i] = maxindex;
-	}
-
-	Z_Free(histogram);
-	Z_Free(source);
-}
-
-
-//
-// R_FlatNumForName
-// Retrieval, get a flat number for a flat name.
-//
-//
-
-int16_t R_FlatNumForName(const char *name)
-{
-	int16_t i = W_GetNumForName(name);
-	return i - firstflat;
+	return colormap[picnum];
 }
 
 
@@ -121,9 +64,5 @@ int16_t R_FlatNumForName(const char *name)
 
 void P_UpdateAnimatedFlat(void)
 {
-	int16_t pic = FLAT_NUKAGE1_COLOR + (((int16_t)_g_leveltime >> 3) % 3) * 2;
-
-	flattranslation[animated_flat_basepic + 0] = pic;
-	flattranslation[animated_flat_basepic + 1] = pic;
-	flattranslation[animated_flat_basepic + 2] = pic;
+	nukage = FLAT_NUKAGE1_COLOR + (((int16_t)_g_leveltime >> 3) % 3) * 2;
 }
