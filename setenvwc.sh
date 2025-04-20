114570a74ece89f138da4a6c6fd71c8907394908
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-04-20 10:48:42 +0200

    libc.lib -> libcm.lib

--- setenvwc.sh
+++ setenvwc.sh
@@ -2,5 +2,5 @@ export ELKSTOPDIR=/home/frenkel/elks
 export WATCOM=/usr/bin/watcom
 
 export INCLUDE=$ELKSTOPDIR/libc/include:$ELKSTOPDIR/include:$ELKSTOPDIR/elks/include:$WATCOM/h:$INCLUDE
-export LIBC=$ELKSTOPDIR/libc/libc.lib
+export LIBC=$ELKSTOPDIR/libc/libcm.lib
 export PATH=$WATCOM/binl64:$WATCOM/binl:$PATH
