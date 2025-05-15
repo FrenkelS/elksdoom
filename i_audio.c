b5d8860c54f6cad9c372417b32d62aaa9aae9a8f
Frenkel Smeijers <frenkel_smeijers@hotmail.com>
2025-05-15 22:53:09 +0200

    remove <math.h>

--- i_audio.c
+++ i_audio.c
@@ -10,7 +10,7 @@
  *  Jess Haas, Nicolas Kalkhof, Colin Phipps, Florian Schulze
  *  Copyright 2005, 2006 by
  *  Florian Schulze, Colin Phipps, Neil Stevens, Andrey Budko
- *  Copyright 2023, 2024 by
+ *  Copyright 2023-2025 by
  *  Frenkel Smeijers
  *
  *  This program is free software; you can redistribute it and/or
@@ -39,7 +39,6 @@
 #include "config.h"
 #endif
 
-#include <math.h>
 #include <stdint.h>
 #ifdef HAVE_UNISTD_H
 #include <unistd.h>
